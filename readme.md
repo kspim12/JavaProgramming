ed0409eb890fafb679a0a366b84a5fdfcde09986
dongryul <dongryul@surem.com>
2021-08-31 22:49:39 +0900

    init commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+#Chapter 3. 연산자
+
+1. 연산자란
+
+2. 결합 방향과 우선순위
+
+3. 연산자의 종류
+ 3.1 산술연산자
+  (1) 산술 연산 시 형변환 
+   ```
+   byte b1 = 10;
+   byte b2 = 20;
+   //byte b3 = b1 + b2; 
+   //Type mismatch: cannot convert from int to byte
+   
+   int i1 = 100;
+   long l1 = 200;
+   //int i2 = i1 + l1;
+   //Type mismatch : cannot convert from long to int
+   ```
+4. 
---
+++ src/main/java/ArithmeticOperatorTest.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperatorTest {
+    public static void main(String[] args) {
+        // 나누기와 나머지 연산자의 차이
+        System.out.println(10 / 3);     // 3: 몫
+        System.out.println(10 % 3 );    // 1 : 나머지
+
+        // 연산 시의 형변환에서 int 이하의 피연산자끼리 연산은 최소 단위가 int
+        //        byte b1 = 10;
+        //        byte b2 = 20;
+        //        //byte b3 = b1 + b2;
+        //        //Type mismatch: cannot convert from int to byte
+        //
+        //        //연산시의 형변환 - int 이상의 피연산자 타입으로 형변환
+        //        int i1 = 100;
+        //        long l1 = 200;
+        //        // int i2 = i1 + l1;
+        // Type mismatch: cannot convert from long to int
+
+        System.out.println(10 / 3);
+        System.out.println(10 / 3.0);
+    }
+}
